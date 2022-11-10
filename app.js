a7b3ba69778b48fef13ab43a553594dcce5c21a5
codewithrasheed <rasheedahmed4845@gmail.com>
2022-11-10 17:25:36 +0500

    JavaScipt Programming

---
+++ app.js
@@ -0,0 +1,138 @@
+// Chapter 16, 17 Practice
+// var a = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// push (last main ek value add karna)
+// pop (last say ek value nikalna)
+// shift (Start say ek value nikalyga)
+// unshift (Start may ek value add kardega)
+// slice (array say value copy karna)
+// splice (array se value cut karna)
+
+// a.push("Rasheed")
+// a.pop();
+// a.shift();
+// a.unshift();
+// var b = a.slice(1,3);
+// var b = a.splice(2,3);
+// console.log(a)
+
+// var b = a.splice(3,0, "1", "2", "3")
+
+// console.log(a);
+// console.log(b);
+
+// var i = a.indexOf()
+// console.log(i);
+
+// var i = a.indexOf("e")
+// console.log(a[i]);
+
+// var i = 4
+// console.log(a[i]);
+
+// var b = a.length;
+// console.log(a,b);
+
+// Chapter 18 Practice
+// for loop Topic Start
+
+// console.log("-------------------------------------")
+// var table = 2
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 3
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 4
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 5
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 6
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 7
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 8
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 9
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 10
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 11
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 12
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 13
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 14
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 15
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 16
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 17
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 18
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 19
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+// var table = 20
+// for (var i = 0; i < 10; i++) {
+// console.log(table, "x", +(i + 1) + " =", +table * (i + 1));
+// }
+// console.log("-------------------------------------")
+
+// every line should be like this
+// 2 X 1 = 2
+
+// x---------------------------x---------------------------x
